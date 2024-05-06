364ff7c96f90f769d882d39eeaec8b1b96e6a1bb
Micah Turner <turner.micah@gmail.com>
2024-05-05 22:09:27 -0600

    look and feel

--- main.lua
+++ main.lua
@@ -10,8 +10,18 @@ love.graphics.setDefaultFilter('nearest', 'nearest')
 MAIN_CHARACTER = '@'
 ENEMY_CHARACTER = '0'
 CHOSEN_1_CHARACTER = '1'
-GROUND_CHARACTERS = { '.', ',', "'", '·', CEDILLA, DEGREES, INTERPUNCT, ACCENT, BACKTICK, ORDINAL }
+GROUND_CHARACTERS = { '.', ',', "'" } --, '·' , CEDILLA, DEGREES, INTERPUNCT, ACCENT, BACKTICK, ORDINAL }
 MAX_HP = 3
+
+COLOR_GUI_TEXT = { 0, 1, 0.976 }
+COLOR_GROUND = { 0.165, .5, 0.29 }
+COLOR_ENEMY_CHARACTERS = { 1, 1, 1 }
+COLOR_ENEMY_EXPLOSION = { 0.929, 0.408, 0.102 }
+COLOR_MAIN_CHAR_EXPLOSION = { 0.624, 0.929, 0.102 }
+COLOR_CHOSEN_1 = { 0.91, 0.855, 0.584 }
+COLOR_BG_MAIN = { .1, .1, .1 }
+COLOR_BG_OTHERSIDE = { .1, .1, .3 }
+COLOR_MAIN_CHAR = { 0, 1, 0 }
 
 local grid = {}
 local gridWidth = 7
@@ -23,11 +33,13 @@ local otherHeight = 5
 local otherRegistry = {}
 local currentPlayerRegistry = registry
 local currentPlayerGrid = grid
-local currentPlayerColor = { 0, 1, 0 }
+local currentPlayerColor = { COLOR_MAIN_CHAR }
 local difficultyLevel = 1
 local playerHp = 0
 local gameResettable = false
 local claimedChosen1 = false
+local points = 0
+local chosen1Location = {}
 
 HURT_DELAY = .3
 local hurtTimer = 0
@@ -171,6 +183,8 @@ local function drawGrid(theGrid, width, height, bgColor, textColor, start)
             DrawMainCharacter()
             -- color the enemies
             DrawEnemies()
+            -- color the chosen 1
+            DrawChosen1()
         end
     end
 end
@@ -183,6 +197,8 @@ local function kill(array, theRegistry, enemyKill)
         -- literally smash them into the GROUND lol
         theRegistry[enemy.x][enemy.y] = getRandomElement(GROUND_CHARACTERS)
         if enemyKill then
+            points = points + 1
+            Shake.startShake(.1, 1)
             Explosions.new(enemy.x, enemy.y, 20, { 1, 0, 0 })
         end
     end
@@ -205,7 +221,7 @@ function love.keyreleased(key)
             MoveCharacterInRegistry(currentPlayerRegistry, MAIN_CHARACTER, { x = 1, y = 1 }, POS)
             PlaceChosen1()
             GenerateEnemies()
-            playerHp = MAX_HP + math.floor(difficultyLevel / 2)
+            playerHp = MAX_HP + math.floor(difficultyLevel / 3)
         end
         return
     end
@@ -235,6 +251,7 @@ function love.keyreleased(key)
         if currentPlayerRegistry == otherRegistry and IsTheChosen1(vector) then
             difficultyLevel = difficultyLevel + 1
             claimedChosen1 = true
+            points = points + 1
             return
         end
         if IsNotObstacle(vector, xlimit, ylimit) then
@@ -252,6 +269,7 @@ function love.load()
     initRegistry(registry, gridWidth, gridHeight)
     initRegistry(otherRegistry, otherWidth, otherHeight)
     -- put the main character in the middle of the grid
+    chosen1Location = { x = math.floor(otherWidth / 2) + 1, y = math.floor(otherHeight / 2) + 1 }
     PlaceMainCharacter()
     currentPlayerRegistry[POS.x][POS.y] = MAIN_CHARACTER
     -- pre-fill the grid from registry
@@ -263,7 +281,7 @@ function love.load()
 end
 
 function PlaceChosen1()
-    otherRegistry[math.floor(otherWidth / 2) + 1][math.floor(otherHeight / 2) + 1] = CHOSEN_1_CHARACTER
+    otherRegistry[chosen1Location.x][chosen1Location.y] = CHOSEN_1_CHARACTER
 end
 
 function PlaceMainCharacter()
@@ -293,13 +311,23 @@ end
 function love.draw()
     Shake.draw()
     -- RENDER THE MAIN GRID
-    drawGrid(grid, gridWidth, gridHeight, { .1, .1, .1 }, { 0, 1, 0 })
+    drawGrid(grid, gridWidth, gridHeight, COLOR_BG_MAIN, COLOR_GROUND)
     -- RENDER THE OTHER SIDE
     local otherGridStart = (gridWidth + 1)
-    drawGrid(otherGrid, otherWidth, otherHeight, { .1, .1, .3 }, { 0, 1, 0 }, otherGridStart)
+    drawGrid(otherGrid, otherWidth, otherHeight, COLOR_BG_OTHERSIDE, COLOR_GROUND, otherGridStart)
     Explosions.draw()
     love.graphics.setColor(.1, .1, .8)
-    love.graphics.print('HP: ' .. playerHp, 0, 10)
+    love.graphics.print(
+        '    CHARGE:' .. playerHp ..
+        '      LEVEL:' .. difficultyLevel ..
+        '     POINTS:' .. points,
+        0,
+        10)
+    love.graphics.print('[SPACEBAR/LCTRL]: shoot adjacent cells\n' ..
+        '[ARROWKEYS]: move cardinally\n' ..
+        '  - Objective: Find the chosen 1',
+        0,
+        6 * CELL_SIZE + 10)
     if gameResettable then
         DrawMenu("Press [SPACEBAR] to reset game!\n[ESCAPE]: Exit")
     end
@@ -333,7 +361,7 @@ function TickTimers(dt)
     if hurtTimer >= 0 then
         hurtTimer = hurtTimer - dt
         if hurtTimer <= 0 then
-            currentPlayerColor = { 0, 1, 0 }
+            currentPlayerColor = COLOR_MAIN_CHAR
         end
     end
 end
@@ -417,16 +445,20 @@ function GameOver()
 end
 
 function ResetGame()
-
+    --TODO: this
 end
 
 function DrawMainCharacter()
     drawCharacter(MAIN_CHARACTER, currentPlayerGrid, POS, currentPlayerColor)
 end
 
+function DrawChosen1()
+    drawCharacter(CHOSEN_1_CHARACTER, otherGrid, chosen1Location, COLOR_CHOSEN_1)
+end
+
 function DrawEnemies()
     for _, enemy in pairs(findAllInRegistry(registry, ENEMY_CHARACTER)) do
-        drawCharacter(ENEMY_CHARACTER, grid, enemy, { 0, 1, 0 })
+        drawCharacter(ENEMY_CHARACTER, grid, enemy, COLOR_ENEMY_CHARACTERS)
     end
 end
 
